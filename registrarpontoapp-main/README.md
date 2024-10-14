2568e9c0787f1ae19b603a47eadf40592cdacbf8
DotViegas <160679178+DotViegas@users.noreply.github.com>
2024-10-14 09:24:56 -0400

    Delete registrarpontoapp-main directory
    apagando pasta por commit errado

--- registrarpontoapp-main/README.md
+++
@@ -1,2 +0,0 @@
-# registrarpontoapp
-Este projeto se destina a um sistema de registro ponto junto com um sistema de gestão de pessoas.
--- registrarpontoapp-main/js/app.js
+++
@@ -1,7 +0,0 @@
-const nome = 'Administrador';
-
-// Seleciona o elemento pelo seletor de classe
-let nameElement = document.querySelector('.name_system');
-
-// Insere o nome no elemento
-nameElement.textContent = nome;
